b9b0961d763fc68100f1eb9c9b8268bfa4c05420
Ethan-OH <osm6227@gmail.com>
2023-11-08 15:12:41 +0900

    설정 화면 기본 세팅

--- lib/view/settings_screen.dart
+++ lib/view/settings_screen.dart
@@ -1,14 +1,66 @@
+import 'package:dorothy/view/dummy_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('설정',),
+        title: const Text(
+          '설정',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 30.h,
+              ),
+              _getSetting(settingText: '이용 약관', screen: const DummyScreen()),
+              SizedBox(
+                height: 20.h,
+              ),
+              _getSetting(settingText: '개인정보 처리방침', screen: const DummyScreen()),
+              SizedBox(
+                height: 20.h,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+    Widget _getSetting({required String settingText, required Widget screen}) {
+    return GestureDetector(
+      onTap: () => Get.to(screen),
+      child: Container(
+        color: Colors.transparent,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              settingText,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20.sp,
+              ),
+            ),
+            Icon(
+              Icons.chevron_right_sharp,
+              size: 35.sp,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
