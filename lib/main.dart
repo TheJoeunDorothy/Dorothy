35eaa338a9fd6b853ae7ddf06ca77a5102829848
Ethan-OH <osm6227@gmail.com>
2023-11-08 14:30:53 +0900

    import 경로 오타 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:dorothy/view/camera.dart';
+import 'package:dorothy/view/camera_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
