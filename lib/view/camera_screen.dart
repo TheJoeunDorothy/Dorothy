3bc18c6f932024d179cb2a5582a7caf0f5186633
kangdeagyu <rkdeorb0124@naver.com>
2023-11-10 17:36:51 +0900

    설정한 영역에 들어오면 얼굴인식 되도록 변경

--- lib/view/camera_screen.dart
+++ lib/view/camera_screen.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 
 import 'package:camera/camera.dart';
+import 'package:dorothy/view/settings_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
 
 class CameraScreen extends StatefulWidget {
@@ -19,6 +22,7 @@ class _CameraScreenState extends State<CameraScreen> {
   InputImage? inputImage;
   FaceDetector? faceDetector;
   bool isStreaming = false;
+  Color myColor = Colors.white;
 
   @override
   void initState() {
@@ -52,10 +56,8 @@ class _CameraScreenState extends State<CameraScreen> {
   }
 
   // 카메라 얼굴 인식
-  // 카메라 얼굴 인식
   void startImageStream() async {
     if (isStreaming) {
-      // 이미 스트림이 시작되었다면 더 이상 진행하지 않습니다.
       return;
     }
     isStreaming = true;
@@ -65,27 +67,34 @@ class _CameraScreenState extends State<CameraScreen> {
         inputImage = _inputImageFromCameraImage(image);
         final List<Face>? faces = await faceDetector?.processImage(inputImage!);
 
-        // 아이콘의 영역을 설정합니다. (아이콘의 크기가 200x200이므로 아이콘의 영역도 이에 맞춰 설정합니다)
+        // 얼굴 영역 설정
         final Rect iconRect = Rect.fromCenter(
-          center: Offset(image.width / 2, image.height / 2), // 아이콘은 화면 중앙에 위치
-          width: 200, // 아이콘의 너비
-          height: 200, // 아이콘의 높이
+          center: Offset(image.width / 2, image.height / 2),
+          width: 280.w,
+          height: 300.h,
         );
 
-        // 비어 있지않으면
+        // 얼굴이 넣어져 있으면
         if (faces != null && faces.isNotEmpty) {
           for (final face in faces) {
-            // 얼굴이 아이콘 영역에 있는지 확인합니다.
-            if (iconRect.overlaps(face.boundingBox)) {
+            // 얼굴 영역에 들어와 있으면
+            if (iconRect.contains(
+                    Offset(face.boundingBox.left, face.boundingBox.top)) &&
+                iconRect.contains(
+                    Offset(face.boundingBox.right, face.boundingBox.bottom))) {
+              myColor = Colors.amber;
+              setState(() {});
               print("얼굴이다!!!!!!!!!!!!");
             } else {
               print("인식 안된다~~~~~~~");
-              stopImageStream(); // 얼굴이 아이콘 영역을 벗어나면 스트림을 중지합니다.
+              myColor = Colors.white;
+              stopImageStream();
             }
           }
         } else {
           print("인식 안된다~~~~~~~");
-          stopImageStream(); // 얼굴이 감지되지 않으면 스트림을 중지합니다.
+          myColor = Colors.white;
+          stopImageStream();
         }
       },
     );
@@ -98,9 +107,12 @@ class _CameraScreenState extends State<CameraScreen> {
       return;
     }
     isStreaming = false;
+
     controller!.stopImageStream();
     // 중지 후 다시 시작하는 코드 추가
     Future.delayed(Duration(seconds: 1), () => startImageStream());
+
+    setState(() {});
   }
 
   // InputImage로 변환
@@ -160,6 +172,27 @@ class _CameraScreenState extends State<CameraScreen> {
       return Container();
     }
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false, // 뒤로가기 제한
+        actions: [
+          IconButton(
+            onPressed: () => Get.defaultDialog(
+                backgroundColor: Colors.white,
+                title: '설명창',
+                actions: [
+                  CupertinoButton.filled(
+                    child: Text('확인'),
+                    onPressed: () => Get.back(),
+                  ),
+                ]),
+            icon: const Icon(Icons.info_outlined),
+          ),
+          IconButton(
+            onPressed: () => Get.to(const SettingsScreen()),
+            icon: const Icon(Icons.settings),
+          ),
+        ],
+      ),
       body: SafeArea(
         child: Stack(
           children: [
@@ -170,72 +203,48 @@ class _CameraScreenState extends State<CameraScreen> {
               ),
             ),
             Center(
-              child: Icon(Icons.face,
-                  size: 200, color: Colors.white.withAlpha(80)),
+              child: Container(
+                width: 230.w, // 사람 얼굴 사이즈에 맞게 조정
+                height: 300.h, // 사람 얼굴 사이즈에 맞게 조정
+                decoration: BoxDecoration(
+                    shape: BoxShape.rectangle, // 모서리 틀로 변경
+                    border: Border.all(color: myColor, width: 5)),
+              ),
             ),
           ],
         ),
       ),
       // 바텀 갤러리, 사진, 보관함
       bottomNavigationBar: Container(
-          color: Colors.white,
-          height: 230.h,
-          child: BottomAppBar(
-            color: Colors.transparent,
-            elevation: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                // 갤러리 사진 등록
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      icon: const Icon(
-                        Icons.photo_camera_back_outlined,
-                      ),
-                      onPressed: () {
-                        //
-                      },
-                    ),
-                    const Text("사진 업로드", style: TextStyle(fontSize: 8)),
-                  ],
-                ),
-                // 카메라 촬영시 예측 페이지
-                SizedBox(
-                  width: 75.w,
-                  height: 75.h,
-                  child: FloatingActionButton(
-                    onPressed: () {
-                      //
-                    },
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.black,
-                    elevation: 0,
-                    shape: const CircleBorder(
-                      side: BorderSide(color: Colors.black, width: 5.0),
-                    ),
-                    child: const Icon(Icons.photo_camera),
+        color: Colors.white,
+        height: 230.h,
+        child: BottomAppBar(
+          color: Colors.transparent,
+          elevation: 0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // 카메라 촬영시 예측 페이지
+              SizedBox(
+                width: 75.w,
+                height: 75.h,
+                child: FloatingActionButton(
+                  onPressed: () {
+                    //
+                  },
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                  elevation: 0,
+                  shape: const CircleBorder(
+                    side: BorderSide(color: Colors.black, width: 5.0),
                   ),
+                  child: const Icon(Icons.photo_camera),
                 ),
-                // 결과 리스트 페이지
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      icon: const Icon(
-                        Icons.list_alt_rounded,
-                      ),
-                      onPressed: () {
-                        //
-                      },
-                    ),
-                    const Text("Dorothy 갤러리", style: TextStyle(fontSize: 3)),
-                  ],
-                ),
-              ],
-            ),
-          )),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
