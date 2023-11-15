0ab91d9c858e55736c64389226f7cacb7ce6ccf4
kangdeagyu <rkdeorb0124@naver.com>
2023-11-15 17:06:40 +0900

    온보딩 디자인 수정 및 서버 null 값 처리

--- lib/view/onboarding_screen.dart
+++ lib/view/onboarding_screen.dart
@@ -10,11 +10,11 @@ class OnBoardingScreen extends StatelessWidget {
 
   List<PageViewModel> getPages(BuildContext context) {
     return [
-      buildPage(
-          context, "assets/images/face_age.jpeg", '당신의 얼굴을 분석하여\n나이를 예측해보세요.'),
-      buildPage(context, "assets/images/personal_color.jpeg",
+      buildPage(context, "assets/images/face_age.jpeg", "얼굴 나이 분석",
+          '당신의 얼굴을 분석하여\n나이를 예측해보세요.'),
+      buildPage(context, "assets/images/personal_color.jpeg", "퍼스널 컬러 분석",
           '당신의 얼굴을 분석하여\n가장 잘 어울리는 컬러를 찾아보세요.'),
-      buildPage(context, "assets/images/face_recognition.jpeg",
+      buildPage(context, "assets/images/face_recognition.jpeg", "얼굴 인식 AI",
           '당신의 얼굴을 카메라로 찍어보세요.\n얼굴을 인식하여 분석을 시작합니다.'),
     ];
   }
@@ -22,15 +22,16 @@ class OnBoardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: IntroductionScreen(
         pages: getPages(context),
         showNextButton: true,
-        next: const Icon(Icons.arrow_forward, color: Colors.white),
+        next: const Icon(Icons.arrow_forward_ios, color: Colors.black),
         done: const Text("시작하기",
-            style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white)),
+            style: TextStyle(fontWeight: FontWeight.w600, color: Colors.black)),
         onDone: () async {
           SharedPreferences prefs = await SharedPreferences.getInstance();
-          await prefs.setBool('hasSeenOnboarding', true);
+          await prefs.setBool('hasSeenOnboarding', false);
           Get.offAll(() => const PermissionScreen());
         },
         dotsDecorator: DotsDecorator(
--- lib/widget/pageview_onbarding.dart
+++ lib/widget/pageview_onbarding.dart
@@ -1,34 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
-PageViewModel buildPage(BuildContext context, String imagePath, String text) {
+PageViewModel buildPage(
+    BuildContext context, String imagePath, String titleText, String text) {
   return PageViewModel(
-    title: "",
-    body: "",
-    image: Stack(
-      children: [
-        Positioned.fill(
-          child: Image.asset(imagePath, fit: BoxFit.fill),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            margin: EdgeInsets.only(
-                bottom: MediaQuery.of(context).size.height * 0.1),
-            child: Text(
-              text,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
-            ),
-          ),
-        ),
-      ],
-    ),
-    decoration: const PageDecoration(
-      fullScreen: true,
-    ),
+    title: titleText,
+    body: text,
+    image: Image.asset(imagePath, fit: BoxFit.fill),
   );
 }
