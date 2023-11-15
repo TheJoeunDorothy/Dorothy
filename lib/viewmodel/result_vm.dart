641c6fe31ede1f0c739bb17b82d909eed6cdd7c3
Ethan-OH <osm6227@gmail.com>
2023-11-15 21:48:46 +0900

    결과이미지 공유 기능 추가

--- lib/viewmodel/result_vm.dart
+++ lib/viewmodel/result_vm.dart
@@ -1,10 +1,27 @@
+import 'package:dorothy/model/logs.dart';
+import 'package:dorothy/model/logs_handler.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class ResultVM extends GetxController{
+  late LogsHandler handler;
   var pageController = PageController();
   late Map<String, dynamic> result;
   late String originalImage;
   /// 페이지뷰 현재 페이지
   RxInt currentPage = 0.obs;
+  GlobalKey key = GlobalKey();
+
+  @override
+  void onInit() {
+    super.onInit();
+    handler = LogsHandler();
+  }
+
+  Future<void> saveLog(base64AgeImage, base64ColorImage) async {
+    Logs log = Logs(originalimage: originalImage, ageimage: base64AgeImage, personalimage: base64ColorImage);
+    await handler.insertLogs(log);
+  }
+
+  
 }
