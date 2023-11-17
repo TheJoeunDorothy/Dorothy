e12d81c82931c6055d7c3e682e4b683d234b9823
Oh-Kang94 <okh19941994@naver.com>
2023-11-17 15:36:58 +0900

    #017 add Auth Settings View

--- lib/view/log_screen.dart
+++ lib/view/log_screen.dart
@@ -9,7 +9,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 
 class LogScreen extends StatefulWidget {
   const LogScreen({super.key});
--- lib/view/settings_screen.dart
+++ lib/view/settings_screen.dart
@@ -1,15 +1,18 @@
 import 'package:dorothy/static/version.dart';
 import 'package:dorothy/view/agreement_screen.dart';
 import 'package:dorothy/view/logs_screen.dart';
+import 'package:dorothy/viewmodel/settings_vm.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
+    final SettingVM controller = Get.put(SettingVM());
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -45,11 +48,45 @@ class SettingsScreen extends StatelessWidget {
             SizedBox(
               height: 30.h,
             ),
-            _getSetting(settingText: '이용 약관', screen: const AgreementScreen()),
+            _getSetting(false,
+                settingText: '이용 약관', screen: const AgreementScreen()),
             SizedBox(
               height: 30.h,
             ),
-            _getSetting(settingText: '사진 기록', screen: const LogsScreen()),
+            _getSetting(true, settingText: '사진 기록', screen: LogsScreen()),
+            SizedBox(
+              height: 30.h,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15.w),
+              child: InkWell(
+                child: Container(
+                  height: 50.h,
+                  color: Colors.transparent,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "잠금 모드",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17.sp,
+                        ),
+                      ),
+                      Obx(
+                        () => CupertinoSwitch(
+                          value: controller.isPrivated.value,
+                          onChanged: (value) {
+                            controller.isPrivated.value = value;
+                            controller.setisPrivatedState(value);
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
@@ -57,11 +94,39 @@ class SettingsScreen extends StatelessWidget {
   }
 
   /// Card 대신 만든 페이지 라우트용 위젯
-  Widget _getSetting({required String settingText, required Widget screen}) {
+  Widget _getSetting(bool isLocked,
+      {required String settingText, required Widget screen}) {
+    final SettingVM controller = Get.find<SettingVM>();
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 15.w),
       child: InkWell(
-        onTap: () => Get.to(()=> screen),
+        onTap: () async {
+          if (isLocked) {
+            // 비밀 기능이 필요한 경우
+            if (controller.isPrivated.value) {
+              // 인증이 필요한 경우
+              if (controller.authorized.value) {
+                Get.to(() => screen);
+              } else {
+                // 인증을 시도
+                try {
+                  await controller.authenticate();
+                  if (controller.authorized.value) {
+                    Get.to(() => screen);
+                  }
+                } catch (e) {
+                  controller.cancelAuthentication();
+                }
+              }
+            } else {
+              // 비밀 기능이 필요하지 않은 경우
+              Get.to(() => screen);
+            }
+          } else {
+            // 비밀 기능이 필요하지 않은 경우
+            Get.to(() => screen);
+          }
+        },
         child: Container(
           height: 50.h,
           color: Colors.transparent,
